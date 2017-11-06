c55812cd485bc2adcff30d1b168e79191eb2321e
Simon Tucker <brotherlogic@gmail.com>
2017-11-05 18:21:56 -0800

    Updated to version 0.37

--- src/main/java/com/github/brotherlogic/serverstatus/Model.java
+++ src/main/java/com/github/brotherlogic/serverstatus/Model.java
@@ -1,5 +1,7 @@
 package com.github.brotherlogic.serverstatus;
 
+import com.github.brotherlogic.javaserver.NetworkObject;
+
 import discovery.Discovery.RegistryEntry;
 import discovery.Discovery.ServiceList;
 import discovery.DiscoveryServiceGrpc;
@@ -7,10 +9,16 @@ import discovery.DiscoveryServiceGrpc;
 public class Model extends NetworkObject {
 	State s;
 
+	String bServer;
+
+	public Model(String baseServer) {
+		bServer = baseServer;
+	}
+
 	// Updates the state of the system
 	private void updateState() {
 		DiscoveryServiceGrpc.DiscoveryServiceBlockingStub service = DiscoveryServiceGrpc
-				.NewBlockingStub(dial("discover"));
+				.NewBlockingStub(dial(bServer, "discover"));
 		ServiceList serviceList = service.listAllServices(discovery.Discovery.Empty.newBuilder().build());
 
 		for (RegistryEntry entry : serviceList.getServicesList()) {
